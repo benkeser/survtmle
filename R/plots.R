06aab163c291b991b67dd9b4507d32543a66879b
Nima Hejazi <nh@nimahejazi.org>
2017-07-06 20:51:14 -0700

    add plot example + more responsive title

--- R/plots.R
+++ R/plots.R
@@ -24,13 +24,30 @@ utils::globalVariables(c("value", "group"))
 #'
 #' @method plot tp.survtmle
 #'
+#' @examples
+#' library(survtmle)
+#' set.seed(341796)
+#' n <- 100
+#' t_0 <- 10
+#' W <- data.frame(W1 = runif(n), W2 = rbinom(n, 1, 0.5))
+#' A <- rbinom(n, 1, 0.5)
+#' T <- rgeom(n,plogis(-4 + W$W1 * W$W2 - A)) + 1
+#' C <- rgeom(n, plogis(-6 + W$W1)) + 1
+#' ftime <- pmin(T, C)
+#' ftype <- as.numeric(ftime == T)
+#' suppressWarnings(
+#'   fit <- survtmle(ftime = ftime, ftype = ftype,
+#'                   adjustVars = W, glm.ftime = "I(W1*W2) + trt + t",
+#'                   trt = A, glm.ctime = "W1 + t", method = "hazard",
+#'                   verbose = TRUE,  t0 = t_0, maxIter = 2)
+#' )
+#' tpfit <- timepoints(fit, times = seq_len(t_0))
+#' plot(tpfit, type = "smooth")
+#'
 plot.tp.survtmle <- function(x, ..., type = c("smooth", "raw")) {
 
   # check that input for type is appropriate
   type <- match.arg(type)
-
-  # capture call for later use
-  call <- match.call(expand.dots = TRUE)
 
   # extract point estimates from tp.survtmle input object and make tidy
   est <- lapply(x, function(x) {x$est})
@@ -57,9 +74,14 @@ plot.tp.survtmle <- function(x, ..., type = c("smooth", "raw")) {
                        ggplot2::aes(x = t, y = value, colour = group)
                       )
   p <- p + ggplot2::geom_line()
-  p <- p + ggplot2::xlab("time point")
-  p <- p + ggplot2::ylab("cumulative incidence")
-  p <- p + ggplot2::ggtitle("cumulative incidence by time")
-  p <- p + ggplot2::theme_minimal()
+  p <- p + ggplot2::xlab("Time")
+  p <- p + ggplot2::ylab("Cumulative Incidence Estimate")
+  p <- p + ggplot2::ggtitle(paste("Cumulative Incidence Amongst Groups",
+                                  ifelse(type == "smooth",
+                                         "\n (smoothed by isotonic regression)",
+                                         "\n (raw estimates)")
+                                  )
+                           )
+  p <- p + ggplot2::theme_bw()
   return(p)
 }
